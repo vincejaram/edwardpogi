eacb57ef9c26beedced6af093ea003a39884e1a2
vincejaram <vincemrj22@gmail.com>
2024-03-09 15:30:45 +0800

    Initial commit

---
+++ git chuchu.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<conio.h>
+#include<windows.h>
+	
+main(){
+	int i, j, primesum, count;
+	
+	for(i=1;i<2000000;i++){
+		for(j=1;j<i/2,j++){
+			if(i%j==0)
+			count++;
+		}
+	}
+	for(i=2 ; i<100 ; i++){
+    count = 0;
+    for (i = 2; i <= num/2; i++)
+    {
+	if(num%i == 0)
+  	  count++;
+    }
+    if(count == 0)
+    {
+	printf(" %d ", num);
+	prime_counter++;
+    }
+
+  }
+	
+	getch();
+}
